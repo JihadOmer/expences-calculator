9b5797b3e50757cbad8aa7eedd900ca120cca852
Jihad Omer <jhjahony@gmail.com>
2022-12-07 09:13:40 +0000

    test commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+    <h1>Hi Jihad I am React, Nice to meet you 😍</h1>
   
     </div>
   );
 }
 
 export default App;
+
+
+
+
